fcf79cd7f60c775b4ed48a522bd0276f3eb79d6a
YukiSakamoto <j.c.ferrero.esp@gmail.com>
2013-04-28 12:58:58 +0900

    introduce boost test library(not headers only, but now, available on only Mac OS X only)

--- core/tests/Position3_test.cpp
+++ core/tests/Position3_test.cpp
@@ -1,7 +1,7 @@
 #define BOOST_TEST_MODULE "Position3_test"
-#define BOOST_TEST_NO_LIB
+//#define BOOST_TEST_NO_LIB
 
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 
 #include "../Position3.hpp"
 #include "../linear_algebra.hpp"
