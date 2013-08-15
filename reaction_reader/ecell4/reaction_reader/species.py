2615d74aa4aea3bbad4f63c6985cef3e7959a46d
Yuki Sakamoto <yukisakamoto06@gmail.com>
2013-08-15 16:32:46 +0900

    substitute Convert2BNGManager for each convert2bng_XXX functions

--- reaction_reader/ecell4/reaction_reader/species.py
+++ reaction_reader/ecell4/reaction_reader/species.py
@@ -953,6 +953,90 @@ def generate_reactions(newseeds, rules, max_iter=10, max_stoich={}):
 
     return seeds + newseeds
 
+def add_modification_collection_dict(self, current_dict, subunit):
+    su_name = subunit.get_name()
+    if not current_dict.has_key( su_name ):
+        current_dict[subunit.get_name()] = {}
+    for mod, (state, binding) in subunit.get_modifications().items():
+        if mod in current_dict[su_name]:
+            current_dict[su_name][mod].append(state)
+        else:
+            current_dict[su_name][mod] = [state]
+    return current_dict
+
+class Convert2BNGManager(object):
+    def __init__(self, species, rules):
+        self.__species = species
+        self.__rules = rules
+        self.__modification_collection_dict = {}
+        if 0 < len(species) and 0 < len(rules):
+            self.build_modification_collection_dict()
+
+    def format_as_seed_species(self, fd):
+        fd.write("begin seed species\n")
+        for i, (sp, attr) in enumerate( self.__species ):
+            fd.write("\t%s\t%f\n" % (sp.convert2bng(), attr))
+        fd.write("end seed species\n")
+
+    def format_as_molecule_types(self, fd):
+        def build_molecules_type_query_list(current_dict):
+            retval = []
+            for su_name in current_dict:
+                mod_list = []
+                for m, state_list in current_dict[su_name].items():
+                    mod = "%s" % m
+                    for state in list(set(state_list)):
+                        if state != '' and state != "_":
+                            mod = "%s~%s" % (mod, state)
+                    mod_list.append(mod)
+                retval.append("%s(%s)" % (su_name, ','.join(mod_list) ))
+            return retval
+
+
+        # write
+        fd.write("begin molecule types\n")
+        for s in build_molecules_type_query_list(self.__modification_collection_dict):
+            fd.write("\t%s\n" % s)
+        fd.write("end molecule types\n")
+
+    def format_as_reaction_rules(self, fd):
+        fd.write("begin reaction rules\n")
+        for i, rr in enumerate(self.__rules):
+            s = "\t%s\t%f" % (rr.convert2bng(),rr.options()[0])
+            #fd.write("\t%s\t%f" % (rr.convert2bng(),rr.options()[0]))
+            for cond in rr.options():
+                if isinstance(cond, Option):
+                    s = "%s %s" % (s, cond.convert2bng() )
+            s += "\n"
+            fd.write(s)
+        fd.write("end reaction rules\n")
+
+    def build_modification_collection_dict(self):
+        def add_modification_collection_dict_subunit(current_dict, subunit):
+            su_name = subunit.get_name()
+            if not current_dict.has_key( su_name ):
+                current_dict[subunit.get_name()] = {}
+            for mod, (state, binding) in subunit.get_modifications().items():
+                if mod in current_dict[su_name]:
+                    current_dict[su_name][mod].append(state)
+                else:
+                    current_dict[su_name][mod] = [state]
+            return current_dict
+        #-------------------------------------------------------------------
+        current_dict = {}
+        reactants = []
+        products = []
+        for rr in self.__rules:
+            reacntants = rr.reactants()
+            products = rr.products()
+            for r in reactants:
+                for su in r.get_subunit_list():
+                    current_dict = add_modification_collection_dict_subunit(current_dict, su)
+            for p in products:
+                for su in p.get_subunit_list():
+                    current_dict = add_modification_collection_dict_subunit(current_dict, su)
+        self.__modification_collection_dict = current_dict
+
 
 def convert2bng_moleculetypes(fd, rules):
     def add_modification_collection_dict(current_dict, subunit):
